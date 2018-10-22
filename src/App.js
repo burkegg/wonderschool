14c1d16ad43929d33f6ec421d685c3f06b998217
Burke <burkegreen@gmail.com>
2018-10-22 12:54:48 -0700

    Basic file and data structure.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { payload } from './data/data';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+    };
+  }
+
+  useHardcoded() {
+    let groups = {};
+    // make a dict of all groups as:  { group: [items] }
+    for (let idx = 0; idx < payload.length; idx++) {
+      let name = payload[idx].group;
+      if (!groups[payload[idx].group]) {
+        groups[name] = [payload[idx]];
+      } else {
+        groups[name] = groups[name].concat(payload[idx])
+      }
+    }
+    this.setState({ taskLists: groups }, () => {console.log(this.state)});
+  }
+
+  componentDidMount() {
+    this.useHardcoded();
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          
         </header>
       </div>
     );
@@ -26,3 +37,4 @@ class App extends Component {
 }
 
 export default App;
+
