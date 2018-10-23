72d2a267abea1b35881ed61a7c64ec6d40254efb
Burke <burkegreen@gmail.com>
2018-10-23 12:45:03 -0700

    Images show correctly.

--- src/Item.js
+++ src/Item.js
@@ -5,39 +5,55 @@ const StyledLocked = styled.div`
   font-size: 1.5em;
   text-align: center;
   color: grey;
+  height: 50;
+  width: 200;
 `;
 
 const StyledUnlocked = styled.div`
   font-size: 1.5em;
   text-align: center;
   color: black;
+  height: 50;
+  width: 200;
 `;
 
 export default function Item(props) {
   const { single, handleTaskClick, taskIsUnlocked } = props;
   const id = single.id;
 
-  if (taskIsUnlocked(single.id)){
-    console.log('unlocked');
-    return (
-    <StyledUnlocked
-    id={id}
-    onClick={(e) => {handleTaskClick(e, id)}}
-    >
-      {single.task}__{id}
-    </StyledUnlocked>
-  )
-  } else {
+  if (!taskIsUnlocked(single.id)){
     console.log('locked');
     return (
     <StyledLocked
     id={id}
     onClick={(e) => {handleTaskClick(e, id)}}
     >
+      <img src='./icons/locked.svg' alt='locked icon' />
       {single.task}__{id}
     </StyledLocked>
   )
   }
 
+  if (single.completedAt !== null){
+    return (
+    <StyledUnlocked
+    id={id} 
+    onClick={(e) => {handleTaskClick(e, id)}}
+    >
+      <img src='./icons/completed.svg' alt='completed icon' />
+      {single.task}__{id}
+    </StyledUnlocked>
+  )
+  } else {
+    return (
+    <StyledUnlocked
+    id={id} 
+    onClick={(e) => {handleTaskClick(e, id)}}
+    >
+      <img src='./icons/incomplete.svg' alt='completed icon' />
+      {single.task}__{id}
+    </StyledUnlocked>
+  )
+  } 
   
 }
--- src/List.js
+++ src/List.js
@@ -1,25 +1,49 @@
 import Item from './Item';
 import React from 'react';
+import styled from 'styled-components';
+
+const ListTopBar = styled.div`
+  
+  font-size: 2em;
+  text-align: left;
+  color: black;
+  height: 50;
+`;
+
+const ReturnView = styled.span `
+  font-size: 1em;
+  padding-left: 50px;
+  color: blue;
+`;
+
+const ToDoList = styled.div `
+  position: absolute;
+
+`;
 
 export default function List(props) {
   const { miniList, name, handleGroupSelect, handleTaskClick, taskIsUnlocked, } = props;
-  console.log(props);
+  const headerText = miniList[0].group;
   return (
-    <div>
-      <div
-        value={name}
-        onClick={(e) => handleGroupSelect(e, name)}>
-      Show all lists
-      </div>
+    <ToDoList>
+      <ListTopBar>
+        {headerText}
+      
+          <ReturnView 
+            value={name}
+            onClick={(e) => handleGroupSelect(e, name)}>
+          Show all lists
+          </ReturnView>
+        </ListTopBar>
+        {miniList.map((single) => (
+          <Item
+            single={single}
+            key={single.id}
+            handleTaskClick={handleTaskClick}
+            taskIsUnlocked={taskIsUnlocked}
+          />
+        ))}
 
-      {miniList.map((single) => (
-        <Item
-          single={single}
-          key={single.id}
-          handleTaskClick={handleTaskClick}
-          taskIsUnlocked={taskIsUnlocked}
-        />
-      ))}
-    </div>
+    </ToDoList>
   )
 };
